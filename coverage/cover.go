7534bd602fb58e1e0a52366ea5842534c0ab15f8
Patrick Stephen <patrick@strongdm.com>
2019-11-30 12:47:28 -0600

    actions: use go libraries in coverage

--- coverage/cover.go
+++ coverage/cover.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 )
@@ -29,33 +30,34 @@ func main() {
 }
 
 func ubuntu() {
-	// cmd := exec.Command("cd", "screen/")
 	wd, err := os.Getwd()
 	fmt.Println("PWD:", wd, err)
 
 	cmd := exec.Command("go", "test", "-coverprofile=profile.out", "-covermode=atomic", "./screen/...")
-	cmd2 := exec.Command("cat", "profile.out", ">>", "coverage.txt")
-	cmd3 := exec.Command("rm", "profile.out")
-	// "cat profile.out >> coverage.txt",
-	// "rm profile.out")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
 	if err != nil {
-		fmt.Println("Erring", err)
-		os.Exit(1)
-	}
-	err2 := cmd2.Run()
-	if err2 != nil {
-		fmt.Println("2", err2)
-		os.Exit(1)
-	}
-	err3 := cmd3.Run()
-	if err3 != nil {
-		fmt.Println("3", err3)
+		fmt.Println("go test err", err)
 		os.Exit(1)
 	}
 
+	prof, err := os.Open("profile.out")
+	if err != nil {
+		fmt.Println("Failed to open profile", err)
+		os.Exit(1)
+	}
+	cov, err := os.OpenFile("coverage.txt", os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Failed to open coverage file", err)
+		os.Exit(1)
+	}
+	written, err := io.Copy(cov, prof)
+	if err != nil {
+		fmt.Println("Failed to copy data", err)
+		os.Exit(1)
+	}
+	fmt.Println("Wrote", written, "bytes")
 }
 func windows() {
 
